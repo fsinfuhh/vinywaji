81d65b8b8b92c2e74e7075bce4c31b5ee40b5cff
Finn-Thorben Sell <dev@finn-thorben.me>
2021-11-11 22:46:22 +0100

    adjust gui to new transaction creation api
    This allows a cleaner form

--- src/bitbots_drinks_gui/static/base.js
+++ src/bitbots_drinks_gui/static/base.js
@@ -1,3 +1,19 @@
+"use strict";
+
+// auto-initialize materialize components
 document.onload = function () {
     M.AutoInit();
 };
+
+// submit all forms via JavaScript to prevent the browser from navigating to the api endpoint
+document.querySelector("form.js-submit").addEventListener("submit", async function (event) {
+    event.preventDefault();
+
+    const data = new FormData(event.target)
+    await fetch(event.target.action, {
+        method: event.target.method,
+        body: data
+    })
+
+    location.reload()
+});
