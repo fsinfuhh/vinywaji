6f17a57afa75475b036a1b97f0c4902f41317e3f
Finn-Thorben Sell <dev@finn-thorben.me>
2021-11-11 23:03:42 +0100

    add form to deposit money

--- src/bitbots_drinks_gui/views.py
+++ src/bitbots_drinks_gui/views.py
@@ -1,3 +1,5 @@
+import math
+
 from django.http import HttpRequest
 from django.views import View
 from django.shortcuts import render
@@ -5,4 +7,13 @@ from django.shortcuts import render
 
 class DashboardView(View):
     def get(self, request: HttpRequest):
-        return render(request, "views/dashboard.html")
+        return render(
+            request,
+            "views/dashboard.html",
+            {
+                "pay_up_amount": 0
+                if request.user.current_balance >= 0
+                else math.ceil(request.user.current_balance / -100),
+                "current_balance": request.user.current_balance / 100,
+            },
+        )
