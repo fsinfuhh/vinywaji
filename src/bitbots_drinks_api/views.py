acd14eadb45be85f7d6f260e3ccadba7d1816f72
Finn-Thorben Sell <dev@finn-thorben.me>
2021-11-11 22:31:51 +0100

    allow currency and type specification during transaction creation

--- src/bitbots_drinks_api/views.py
+++ src/bitbots_drinks_api/views.py
@@ -1,7 +1,7 @@
-from drf_spectacular.utils import extend_schema, inline_serializer
+from drf_spectacular.utils import extend_schema, inline_serializer, OpenApiParameter
 from rest_framework.request import Request
 from rest_framework.response import Response
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework import views
 from rest_framework.decorators import action
 from django.conf import settings
@@ -30,7 +30,13 @@ class UserViewSet(viewsets.ReadOnlyModelViewSet):
         return Response(serializer.data)
 
 
-class TransactionViewSet(viewsets.ModelViewSet):
+class TransactionViewSet(
+    viewsets.mixins.CreateModelMixin,
+    viewsets.mixins.RetrieveModelMixin,
+    viewsets.mixins.ListModelMixin,
+    viewsets.mixins.DestroyModelMixin,
+    viewsets.GenericViewSet,
+):
     serializer_class = serializers.TransactionSerializer
 
     def get_queryset(self):
@@ -43,6 +49,42 @@ class TransactionViewSet(viewsets.ModelViewSet):
         if self.action == "list":
             return [(permissions.IsAuthenticated | permissions.IsServiceAccount)()]
         return [(permissions.IsAdminUser | permissions.IsServiceAccount | permissions.IsRelatedToRequester)()]
+
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                "currency",
+                str,
+                enum=["ct", "euro"],
+                description="Currency in which the provided transaction amount is. If euro, it will be converted to cents automatically.",
+            ),
+            OpenApiParameter(
+                "type",
+                str,
+                enum=["purchase", "deposit"],
+                description="Type of transaction. If not given, negative transactions count as purchases ans positive ones as deposits.",
+            ),
+        ]
+    )
+    def create(self, request: Request, *args, **kwargs):
+        # perform preliminary validation to make sure that later data modification does not fail unexpectedly
+        self.get_serializer(data=request.data).is_valid(raise_exception=True)
+
+        # force QueryDict to be mutable
+        data = request.data.copy()
+
+        # adjust requested transaction object according to query parameters
+        if "currency" in request.query_params and request.query_params["currency"] == "euro":
+            data["amount"] *= 100
+        if "type" in request.query_params and request.query_params["type"] == "purchase":
+            data["amount"] *= -1
+
+        # create the transaction object
+        serializer = self.get_serializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
 
 class AppSettingsViewSet(views.APIView):
