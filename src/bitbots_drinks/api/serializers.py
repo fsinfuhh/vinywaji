b2803b5fe54f3f7ed3bb1bd6c00af884a6336a95
Jonas Kühn <17148798+JOKUE2002@users.noreply.github.com>
2023-10-25 12:21:12 +0200

    Min values angepasst + api-bugfix

--- src/bitbots_drinks/api/serializers.py
+++ src/bitbots_drinks/api/serializers.py
@@ -6,14 +6,9 @@ from bitbots_drinks.core import models
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.User
-        fields = ["id", "transactions", "username", "auth_token", "current_balance"]
+        fields = ["id", "transactions", "username", "current_balance"]
 
     transactions = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
-    auth_token = serializers.SerializerMethodField()
-
-    def get_auth_token(self, instance: "models.User") -> str:
-        return instance.get_or_create_auth_token().key
-
 
 class TransactionSerializer(serializers.ModelSerializer):
     class Meta:
