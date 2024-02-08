d5461e3f2629bc8a1476770924b13fb6b6d4e2b7
ftsell <dev@ftsell.de>
2024-02-08 17:01:24 +0100

    upgrade dependencies

--- src/vinywaji/settings.py
+++ src/vinywaji/settings.py
@@ -63,6 +63,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "simple_openid_connect.integrations.django.middleware.TokenVerificationMiddleware",
 ]
 
 ROOT_URLCONF = "vinywaji.urls"
